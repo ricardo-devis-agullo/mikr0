bfd19f7e446f0a96336d7dc1c187e1a368cc879f
Ricardo Devis Agullo <mkredz@gmail.com>
2024-10-26 22:50:36 +0200

    remove actions

--- packages/mikr0/src/vite/plugins.ts
+++ packages/mikr0/src/vite/plugins.ts
@@ -14,6 +14,8 @@ export const ocClientPlugin = (opts: { entry: string }): PluginOption => {
 				delete mod.exports.default.$args[0].parameters;
 				// biome-ignore lint/performance/noDelete: required for tree shaking
 				delete mod.exports.default.$args[0].plugins;
+				// biome-ignore lint/performance/noDelete: required for tree shaking
+				delete mod.exports.default.$args[0].actions;
 				return generateCode(mod);
 			}
 		},
