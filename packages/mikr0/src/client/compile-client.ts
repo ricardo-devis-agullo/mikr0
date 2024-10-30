3c40a5e85086cb6a85f315fb1659a188dc99453a
Ricardo Devis Agullo <mkredz@gmail.com>
2024-10-30 23:48:34 +0100

    improve performance of compile client

--- packages/mikr0/src/client/compile-client.ts
+++ packages/mikr0/src/client/compile-client.ts
@@ -1,17 +1,7 @@
 import path from "node:path";
-import { buildSync } from "esbuild";
+import { buildSync, transformSync } from "esbuild";
 
 export function compileClient() {
-	const {
-		outputFiles: [clientMinOutput],
-	} = buildSync({
-		bundle: true,
-		minify: true,
-		write: false,
-		target: ["chrome80", "firefox80", "safari12"],
-		entryPoints: [path.join(import.meta.dirname, "./client.js")],
-		format: "iife",
-	});
 	const {
 		outputFiles: [clientOutput],
 	} = buildSync({
@@ -22,11 +12,12 @@ export function compileClient() {
 		entryPoints: [path.join(import.meta.dirname, "./client.js")],
 		format: "iife",
 	});
-	const clientMin = clientMinOutput?.text;
 	const client = clientOutput?.text;
-	if (!clientMin || !client) {
+	if (!client) {
 		throw new Error("Missing client");
 	}
+
+	const { code: clientMin } = transformSync(client, { minify: true });
 
 	return { minified: clientMin, code: client };
 }
