0545cac810893293848309e677703815381dd5bc
Ricardo Devis Agullo <mkredz@gmail.com>
2024-11-04 22:32:28 +0100

    fix memory storage

--- packages/mikr0/src/storage/memory.ts
+++ packages/mikr0/src/storage/memory.ts
@@ -1,26 +1,47 @@
 import fsp from "node:fs/promises";
+import path from "node:path";
 import url from "node:url";
+import { fs } from "memfs";
 import type { StaticStorage } from "./storage.js";
+import { getMimeType } from "./utils.js";
 
 export function MemoryStorage(): StaticStorage {
 	const storage: Map<string, string> = new Map();
 
 	return {
 		async save(src: string, destination: string) {
-			storage.set(destination, await fsp.readFile(src, "utf-8"));
+			for await (const file of fsp.glob(path.join(src, "/*"))) {
+				fs.mkdirSync(path.join("/", destination), { recursive: true });
+        const mime = getMimeType(file);
+        const encoding = mime?.startsWith('image') ? 'binary' : 'utf-8';
+				const data = await fsp.readFile(file, encoding);
+				fs.writeFileSync(
+					path.join("/", destination, path.basename(file)),
+					data,
+          {
+            encoding
+          }
+				);
+			}
 		},
 		async saveFile(destination: string, contents: string) {
+      const mime = getMimeType(destination);
+      const encoding = mime?.startsWith('image') ? 'binary' : 'utf-8';
+			await fs.writeFileSync(path.join("/", destination), contents, {
+				encoding
+			});
 			storage.set(destination, contents);
 		},
-		async get(filePath: string) {
-			const file = storage.get(filePath);
-			if (!file) {
-				throw new Error(`File not found: ${filePath}`);
-			}
-			return file;
+		async get(file: string) {
+      const mime = getMimeType(file);
+      const encoding = mime?.startsWith('image') ? 'binary' : 'utf-8';
+
+			return fs.readFileSync(path.join("/", file), {
+				encoding,
+			}) as string;
 		},
 		getUrl(file: string) {
-			return url.pathToFileURL(file);
+			return url.pathToFileURL(path.join("/", file));
 		},
 	};
 }
