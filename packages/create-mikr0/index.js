026b8ec9a3a6c4dbc25220e3c4c2a019ef844843
Ricardo Devis Agullo <mkredz@gmail.com>
2024-11-04 15:28:20 +0100

    fix template builds

--- packages/create-mikr0/index.js
+++ packages/create-mikr0/index.js
@@ -80,7 +80,7 @@ async function createRegistry() {
 
 	/** @type Record<string, string> */
 	const dependencies = {
-		mikr0: "0.0.12",
+		mikr0: "0.0.13",
 	};
 	if (database === "sqlite") {
 		dependencies.sqlite3 = "5.1.7";
