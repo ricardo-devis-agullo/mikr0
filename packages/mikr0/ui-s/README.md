becc3cd19e9a6831500bbd0825581b65442c0c6c
Ricardo Devis Agullo <mkredz@gmail.com>
2024-11-05 11:59:31 +0100

    remove solid

--- packages/mikr0/ui-s/README.md
+++
@@ -1,5 +0,0 @@
-# Mikr0
-
-Mikr0 is a micro-frontend library.
-
-This repository contains the Mikr0 source code. STILL IN PRODUCTION
--- packages/mikr0/ui-s/src/libs/cn.ts
+++
@@ -1,6 +0,0 @@
-import clsx, { type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...classLists: ClassValue[]) {
-	return twMerge(clsx(classLists));
-}
