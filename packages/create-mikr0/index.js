4092096d010def286c497e53e28037b385bee377
Ricardo Devis Agullo <mkredz@gmail.com>
2024-12-25 12:51:48 +0100

    pub

--- packages/create-mikr0/index.js
+++ packages/create-mikr0/index.js
@@ -80,7 +80,7 @@ async function createRegistry() {
 
 	/** @type Record<string, string> */
 	const dependencies = {
-		mikr0: "0.1.8",
+		mikr0: "0.1.9",
 	};
 	if (database === "sqlite") {
 		dependencies.sqlite3 = "5.1.7";
